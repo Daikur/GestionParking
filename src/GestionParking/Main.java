197056189f68b7786a82771b88fdc09521dd92dd
daw1 <daw1@172.16.201.11>
2016-01-25 10:35:07 +0100

    v2.0

--- src/GestionParking/Main.java
+++ src/GestionParking/Main.java
@@ -1,36 +1,28 @@
 package GestionParking;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        
-        Coche c1 = new Coche("123123", "A123123", "largo");
-        c1.setModelo("Ford Fiesta");
-        c1.setColor("Azul");
-        Moto m1 = new Moto("1231", "MICASA", 3);
-        Coche c2 = new Coche("31412", "CASA", "hola");
-        System.out.println(m1.getNumruedas());
-        System.out.println(c2.getTipo());
-        System.out.println(c2.setTipo("Corto"));
-        System.out.println(c2.getTipo());
 
         Parking pk1 = new Parking("MCDonald");
-        Plaza p1 = new Plaza(1, 1);
-        Plaza p2 = new Plaza(1, 2);
-        Plaza p3 = new Plaza(1, 3);
-        Plaza p4 = new Plaza(1, 4);
-        Plaza p5 = new Plaza(2, 1);
-        Plaza p6 = new Plaza(2, 2);
-        Plaza p7 = new Plaza(2, 3);
-        Plaza p8 = new Plaza(2, 4);
-        
-//        pk1.setAlquiler((String) p1.getSotano()+ (String) p1.getNumplaza(), p1);
-        
-        
-        p2.setVehiculo(c2);
-        System.out.println(c1.toString());
-        System.out.println(p2.getVehiculo());
-        System.out.println(p2.toString());
+
+        Map<String, Integer> plazas = new HashMap<>();
+
+        plazas.put("a", 1);
+        plazas.put("b", 2);
+        plazas.put("c", 1);
+        plazas.put("d", 1);
+        plazas.put("e", 1);
+
+        Iterator<String> it = plazas.keySet().iterator();
+        while (it.hasNext()) {
+            String ssn = it.next();
+            System.out.println(ssn + " " + plazas.get(ssn));
+        }
 
     }
 }
--- src/GestionParking/Parking.java
+++ src/GestionParking/Parking.java
@@ -1,15 +1,45 @@
 package GestionParking;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Parking {
+
     private String nombreparking;
     private String direccion;
-    private Map<String, Plaza> alquiler = new HashMap<>();
-    
+    private Map<String, Plaza> plazas = new HashMap<>();
+
     public Parking(String nombreparking) {
         this.nombreparking = nombreparking;
+        Plaza p1 = new Plaza(1, 1);
+        p1.setTipo('M');
+        p1.setVehiculo(new Moto("1231", "C123512", 3));
+        Plaza p2 = new Plaza(1, 2);
+        p2.setTipo('M');
+        Plaza p3 = new Plaza(1, 3);
+        p3.setTipo('C');
+        p3.setVehiculo(new Coche("123123", "A123123", "Largo"));
+        Plaza p4 = new Plaza(1, 4);
+        p4.setTipo('C');
+        Plaza p5 = new Plaza(2, 1);
+        p5.setTipo('C');
+        p5.setVehiculo(new Coche("31412", "B12312", "Corto"));
+        Plaza p6 = new Plaza(2, 2);
+        p6.setTipo('C');
+        Plaza p7 = new Plaza(2, 3);
+        p7.setTipo('C');
+        Plaza p8 = new Plaza(2, 4);
+        p8.setTipo('M');
+        p8.setVehiculo(new Moto("123514", "D123122", 2));
+        plazas.put("11", p1);
+        plazas.put("12", p2);
+        plazas.put("13", p3);
+        plazas.put("14", p4);
+        plazas.put("21", p5);
+        plazas.put("22", p6);
+        plazas.put("23", p7);
+        plazas.put("24", p8);
     }
 
     public String getNombreparking() {
@@ -28,11 +58,23 @@ public class Parking {
         this.direccion = direccion;
     }
 
-    public Map<String, Plaza> getAlquiler() {
-        return alquiler;
+    public Map<String, Plaza> getPlazas() {
+        return plazas;
     }
 
-    public void setAlquiler(Map<String, Plaza> alquiler) {
-        this.alquiler = alquiler;
-    } 
+    public void setPlazas(Map<String, Plaza> plazas) {
+        this.plazas = plazas;
+    }
+
+//    public String alquilar(Vehiculo vehiculo) {
+//
+//        do {
+//            Iterator<String> it = plazas.keySet().iterator();
+//            while(it.hasNext()) {
+//                String ssn = it.next();
+//                
+//            }
+//            
+//        } while (plazas.get(Plaza) != null);
+//    }
 }
